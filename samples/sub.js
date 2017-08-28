a82052fec79e3dda9c1a00499864b5592556c963
Tomotaka Ito <tomotaka.ito@gmail.com>
2017-08-29 08:55:01 +0900

    better sub.js sample

--- samples/sub.js
+++ samples/sub.js
@@ -23,6 +23,32 @@ conn.connect(function() {
   console.log("@@@ connected");
   var listenerId = conn.addListener(device, function(data) {
     console.log("@@@@ data retrieved");
+
+    var deviceName = data.getDevice().getName();
+
+    console.log("--------- API1: list of TransducerValue");
+    var values = data.getTransducerValues();
+    var i = 0;
+
+    for (i = 0; i < values.length; i++) {
+      var v = values[i];
+      console.log("id=" + v.getTransducerId() + ", rawValue=" + v.getRawValue() + ", typedValue=" + v.getTypedValue());
+    }
+
+    console.log("--------- API2: get id => rawValue mapping object");
+    var id2rv = data.getRawValues();
+    var ids = Object.keys(id2rv);
+    for (i = 0; i < ids.length; i++) {
+      var tid = ids[i];
+      console.log("" + tid + " => raw=" + id2rv[tid]);
+    }
+
+    console.log("--------- API3: get id => typedValue mapping object");
+    var id2tv = data.getTypedValues();
+    for (i = 0; i < ids.length; i++) {
+      var tid = ids[i];
+      console.log("" + tid + " => typed=" + id2tv[tid]);
+    }
   });
   console.log("@@@ listener ID = " + listenerId);
   conn.subscribe(device);
